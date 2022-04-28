7e7273d7dfb5e8fe903d03e238c7d4d9a6d0e141
Leeseunghwan7305 <sun1238799@naver.com>
2022-04-28 21:29:06 +0900

    2섹션 끝

--- 섹션2/봉우리.js
+++ 섹션2/봉우리.js
@@ -1,7 +1,31 @@
 function solution(arr) {
-  const arr2 = new Array(5).fill(0).map(() => new Array(4));
-
-  console.log(arr2);
+  let n = arr.length;
+  let result = 0;
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, 1, 0, -1];
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      let flag = 1;
+      for (let k = 0; k < 4; k++) {
+        let nx = i + dx[k];
+        let ny = j + dy[k];
+        if (
+          nx >= 0 &&
+          nx < n &&
+          ny >= 0 &&
+          ny < n &&
+          arr[nx][ny] >= arr[i][j]
+        ) {
+          flag = 0;
+          break;
+        }
+      }
+      if (flag) {
+        result++;
+      }
+    }
+  }
+  return result;
 }
 
 let arr = [
