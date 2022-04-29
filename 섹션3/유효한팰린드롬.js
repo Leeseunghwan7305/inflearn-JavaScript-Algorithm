e1bd081197a08303c86d33d349a1c87662b4a3b1
Leeseunghwan7305 <sun1238799@naver.com>
2022-04-29 15:37:41 +0900

    [add] 섹션 3

--- 섹션3/유효한팰린드롬.js
+++ 섹션3/유효한팰린드롬.js
@@ -1,4 +1,17 @@
-function solution(s) {}
+function solution(s) {
+  s = s.toLowerCase();
+  s = s.replace(/[^a-z]/g, "");
+  console.log(s);
+  let len = s.length;
+  let answer = "Yes";
+  for (let i = 0; i < Math.floor(len / 2); i++) {
+    if (s[i] != s[len - i - 1]) {
+      answer = "No";
+      return answer;
+    }
+  }
+  return answer;
+}
 
 let str = "found7, time: study; Yduts; emit, 7Dnuof";
 console.log(solution(str));
