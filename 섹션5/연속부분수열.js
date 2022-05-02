b2a257d4a8309903d6b384dd4cccb71160c51b63
Leeseunghwan7305 <sun1238799@naver.com>
2022-05-02 23:46:36 +0900

    5섹션

--- 섹션5/연속부분수열.js
+++ 섹션5/연속부분수열.js
@@ -3,23 +3,18 @@ function solution(m, arr) {
   let pR = 0;
   let sum = 0;
   let num = 0;
-  let len = arr.length;
-
-  while (pL != len && pR != len) {
-    sum = 0;
-    for (let j = pL; j <= pR; j++) {
-      sum += arr[j];
-    }
-    if (sum > 6) {
-      pL++;
-    } else if (sum < 6) {
-      pR++;
-    } else {
+  for (let pR = 0; pR < arr.length; pR++) {
+    sum += arr[pR];
+    if (sum == m) {
       num++;
-      pR++;
+    }
+    while (sum >= m) {
+      sum -= arr[pL++];
+
+      if (sum == m) num++;
     }
   }
-  return num;
+  console.log(num);
 }
 
 let a = [1, 2, 1, 3, 1, 1, 1, 2];
--- 섹션5/연속부분수열2.js
+++ 섹션5/연속부분수열2.js
@@ -1,6 +1,34 @@
-function solution(m, arr){
-   
+function solution(m, arr) {
+  let pL = 0;
+  let pR = 0;
+  let sum = 0;
+  let len = arr.length;
+  let num = 0;
+  while (pL != arr.length && pR != arr.length) {
+    sum = 0;
+    for (let i = pL; i <= pR; i++) {
+      sum += arr[i];
+    }
+    console.log(sum);
+    if (sum <= 5) {
+      pR++;
+      num++;
+    } else {
+      pL++;
+    }
+  }
+  while (pL <= arr.length) {
+    pL++;
+    if (sum <= 5) {
+      num++;
+    }
+  }
+  while (pR <= arr.length) {
+    pR++;
+    if (sum <= 5) num++;
+  }
+  console.log(num);
 }
 
-let a=[1, 3, 1, 2, 3];
+let a = [1, 3, 1, 2, 3];
 console.log(solution(5, a));
