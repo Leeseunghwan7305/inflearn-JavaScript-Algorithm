bc6af02e890c138cd03b7ac1e27e294880899a58
Leeseunghwan7305 <sun1238799@naver.com>
2022-05-10 23:44:00 +0900

    5섹션

---
+++ 섹션5/최대매출.js
@@ -0,0 +1,19 @@
+function solution(k, arr) {
+  let pL = 0;
+  let sum = 0;
+  let max = 0;
+  for (pL = 0; pL < arr.length - k; pL++) {
+    let pR = pL + k - 1;
+    sum = 0;
+    for (let i = pL; i <= pR; i++) {
+      sum += arr[i];
+    }
+    if (sum > max) {
+      max = sum;
+    }
+  }
+  return max;
+}
+
+let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+console.log(solution(3, a));
