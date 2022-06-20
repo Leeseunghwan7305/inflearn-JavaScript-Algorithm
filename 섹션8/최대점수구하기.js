006a478433ed2b44681004fd86af6fa5777169c9
seunghwan <sun1238799@naver.com>
2022-06-21 00:49:24 +0900

    최대점수구하기

--- 섹션8/최대점수구하기.js
+++ 섹션8/최대점수구하기.js
@@ -1,4 +1,24 @@
-function solution(m, ps, pt) {}
+function solution(m, ps, pt) {
+  let ch = Array.from({ length: ps.length }, () => 0);
+  console.log(ch);
+
+  let max = 0;
+  let result = 0;
+  function DFS(n, time, score) {
+    if (time > m || n > ch.length) {
+      return;
+    }
+    if (n == ch.length) {
+      max = Math.max(max, score);
+    }
+
+    DFS(n + 1, time + pt[n], score + ps[n]);
+
+    DFS(n + 1, time, score);
+  }
+  DFS(0, 0, 0);
+  console.log(max);
+}
 
 let ps = [10, 25, 15, 6, 7];
 let pt = [5, 12, 8, 3, 4];
