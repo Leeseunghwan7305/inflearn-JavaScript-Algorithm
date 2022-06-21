01c11e676e612cb46086c2ef6c5c8df99f71e0ce
seunghwan <sun1238799@naver.com>
2022-06-22 00:02:33 +0900

    중복순열

--- 섹션8/중복순열.js
+++ 섹션8/중복순열.js
@@ -1,3 +1,17 @@
-function solution(n, m) {}
+function solution(n, m) {
+  let ch = Array.from({ length: m }, () => 0);
+
+  function DFS(L) {
+    if (L == m) {
+      console.log(ch);
+      return;
+    }
+    for (let i = 1; i <= n; i++) {
+      ch[L] = i;
+      DFS(L + 1);
+    }
+  }
+  console.log(DFS(0));
+}
 
 console.log(solution(3, 2));
