597332cd97949048b435444df7ac9880700e3f1a
seunghwan <sun1238799@naver.com>
2022-06-28 01:40:50 +0900

    조합의경우수

--- 섹션8/조합의경우수.js
+++ 섹션8/조합의경우수.js
@@ -1,3 +1,17 @@
-function solution(n, r) {}
+function solution(n, r) {
+  let answer = [];
+  let ch = Array.from(Array(30), () => Array(30).fill(0));
+
+  function DFS(n, r) {
+    if (ch[n][r] > 0) return ch[n][r];
+    if (r == 0 || n == r) {
+      return 1;
+    } else {
+      return (ch[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+    }
+  }
+  answer = DFS(n, r);
+  return answer;
+}
 
 console.log(solution(5, 3));
