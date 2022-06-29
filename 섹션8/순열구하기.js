fa2c72fd9a2620d073287a844a94e7bff8bdb366
seunghwan <sun1238799@naver.com>
2022-06-29 17:21:18 +0900

    조합의 경우수

--- 섹션8/순열구하기.js
+++ 섹션8/순열구하기.js
@@ -1,14 +1,15 @@
 function solution(m, arr) {
-  let answer = [];
-  n = arr.length;
-  let ch = Array.from({ length: n }, () => 0);
+  let ch = Array.from({ length: arr.length }, () => 0);
   let tmp = Array.from({ length: m }, () => 0);
+
+  console.log(tmp);
+  console.log(ch);
   function DFS(L) {
-    if (L === m) {
-      answer.push(tmp.slice());
+    if (L == tmp.length) {
+      console.log(tmp);
     } else {
-      for (let i = 0; i < n; i++) {
-        if (ch[i] === 0) {
+      for (let i = 0; i < arr.length; i++) {
+        if (ch[i] == 0) {
           ch[i] = 1;
           tmp[L] = arr[i];
           DFS(L + 1);
@@ -18,8 +19,10 @@ function solution(m, arr) {
     }
   }
   DFS(0);
-  return answer;
 }
 
 let arr = [3, 6, 9];
 console.log(solution(2, arr));
+
+//2개 숫자 == tmp ==2개
+//입력하는예제 3개 == ch == 3개
--- 섹션8/조합의경우수.js
+++ 섹션8/조합의경우수.js
@@ -1,17 +1,19 @@
 function solution(n, r) {
-  let answer = [];
-  let ch = Array.from(Array(30), () => Array(30).fill(0));
+  let dy = Array.from(Array(30), () => Array(30).fill(0));
+  console.log(dy);
 
   function DFS(n, r) {
-    if (ch[n][r] > 0) return ch[n][r];
-    if (r == 0 || n == r) {
+    if (dy[n][r] > 0) return dy[n][r];
+    if (r == n || r == 0) {
       return 1;
     } else {
-      return (ch[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
+      return (dy[n][r] = DFS(n - 1, r - 1) + DFS(n - 1, r));
     }
   }
-  answer = DFS(n, r);
+  let answer = DFS(5, 3);
   return answer;
 }
+
+// n 과 r 을 기록할 2차원 배열을 만들어야한다.
 
 console.log(solution(5, 3));
