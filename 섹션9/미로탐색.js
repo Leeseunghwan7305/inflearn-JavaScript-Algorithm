dfd631ba829353cf8a359346ff798a4f27a5a896
seunghwan <sun1238799@naver.com>
2022-07-04 04:22:58 +0900

    미로탐색

--- 섹션9/미로탐색.js
+++ 섹션9/미로탐색.js
@@ -1,4 +1,32 @@
-function solution(board) {}
+function solution(board) {
+  let dx = [-1, 0, 1, 0];
+  let dy = [0, -1, 0, 1];
+  let count = 0;
+  function DFS(x, y) {
+    if (x == 6 && y == 6) {
+      count++;
+      return;
+    } else {
+      for (let i = 0; i < 4; i++) {
+        // 좌표를 넘어가면 안됨
+        if (
+          x + dx[i] >= 0 &&
+          x + dx[i] <= 6 &&
+          y + dy[i] >= 0 &&
+          y + dy[i] <= 6 &&
+          board[x + dx[i]][y + dy[i]] == 0
+        ) {
+          board[x + dx[i]][y + dy[i]] = 1;
+          DFS(x + dx[i], y + dy[i]);
+          board[x + dx[i]][y + dy[i]] = 0;
+        }
+      }
+    }
+  }
+  board[0][0] = 1;
+  DFS(0, 0);
+  console.log(count);
+}
 
 let arr = [
   [0, 0, 0, 0, 0, 0, 0],
